70963dd936337a18f9a4cea85bb134cb1f4f3e59
Simana <135211827+atomac19@users.noreply.github.com>
2025-04-30 20:08:40 +0200

    Finish App component and App style file

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,22 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
-import Business from './Business';
 import BusinessList from './BusinessList';
+import SearchBar from "./SearchBar";
+
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <div class="app">
-      
-        
-        <BusinessList />
-      
+    <>
+    <div class="searchbar">
+      <SearchBar />
     </div>
+    <div class="cards">  
+        <BusinessList />
+    </div>
+    </>
   )
 }
 
-export default App
+export default App;
