d11d84ac9f2d3ad58f217ea2b2576ffb5a08953d
Simana <135211827+atomac19@users.noreply.github.com>
2025-04-30 20:14:48 +0200

    Update BusinessList.jsx

--- src/BusinessList.jsx
+++ src/BusinessList.jsx
@@ -12,25 +12,83 @@ const businessObjectsList = [
         category: 'Italian',
         rating: 4.5,
         reviewCount: 90
+    },
+    {
+        imageSrc: 'https://content.codecademy.com/programs/react/ravenous/pizza.jpg',
+        name: 'MarginOtto Pizzeria',
+        address: '1010 Paddington Way',
+        city: 'Flavortown',
+        state: 'NY',
+        zipCode: '10101',
+        category: 'Italian',
+        rating: 4.5,
+        reviewCount: 90
+    },
+    {
+        imageSrc: 'https://content.codecademy.com/programs/react/ravenous/pizza.jpg',
+        name: 'MarginOtto Pizzeria',
+        address: '1010 Paddington Way',
+        city: 'Flavortown',
+        state: 'NY',
+        zipCode: '10101',
+        category: 'Italian',
+        rating: 4.5,
+        reviewCount: 90
+    },
+    {
+        imageSrc: 'https://content.codecademy.com/programs/react/ravenous/pizza.jpg',
+        name: 'MarginOtto Pizzeria',
+        address: '1010 Paddington Way',
+        city: 'Flavortown',
+        state: 'NY',
+        zipCode: '10101',
+        category: 'Italian',
+        rating: 4.5,
+        reviewCount: 90
+    },
+    {
+        imageSrc: 'https://content.codecademy.com/programs/react/ravenous/pizza.jpg',
+        name: 'MarginOtto Pizzeria',
+        address: '1010 Paddington Way',
+        city: 'Flavortown',
+        state: 'NY',
+        zipCode: '10101',
+        category: 'Italian',
+        rating: 4.5,
+        reviewCount: 90
+    },
+    {
+        imageSrc: 'https://content.codecademy.com/programs/react/ravenous/pizza.jpg',
+        name: 'MarginOtto Pizzeria',
+        address: '1010 Paddington Way',
+        city: 'Flavortown',
+        state: 'NY',
+        zipCode: '10101',
+        category: 'Italian',
+        rating: 4.5,
+        reviewCount: 90
     }
 ]
 
 function BusinessList() {
     return (
         <>
-            <Business 
-                        imageSrc={businessObjectsList[0].imageSrc}
-                        name={businessObjectsList[0].name}
-                        address={businessObjectsList[0].address}
-                        city={businessObjectsList[0].city}
-                        state={businessObjectsList[0].state}
-                        zipCode={businessObjectsList[0].zipCode}
-                        category={businessObjectsList[0].category}
-                        rating={businessObjectsList[0].rating}
-                        reviewCount={businessObjectsList[0].reviewCount}
-                        
-                        
-                        />
+            {
+                businessObjectsList.map(pizzeria => (
+                    <Business 
+                        imageSrc={pizzeria.imageSrc}
+                        name={pizzeria.name}
+                        address={pizzeria.address}
+                        city={pizzeria.city}
+                        state={pizzeria.state}
+                        zipCode={pizzeria.zipCode}
+                        category={pizzeria.category}
+                        rating={pizzeria.rating}
+                        reviewCount={pizzeria.reviewCount}
+                    />
+                ))
+            }
+            
         </>
     );
 }
